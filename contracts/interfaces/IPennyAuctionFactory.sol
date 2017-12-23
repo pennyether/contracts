59913e41a9b83cb9b39db1c998bb7f9904e7befb
Penny Ether Team <pennyether@users.noreply.github.com>
2017-12-23 23:55:24 +0000

    - Removed .createAuction from PAF interface

--- contracts/interfaces/IPennyAuctionFactory.sol
+++ contracts/interfaces/IPennyAuctionFactory.sol
@@ -1,11 +1,4 @@
 pragma solidity ^0.4.19;
 interface IPennyAuctionFactory {
-	// function createAuction(
-	// 	uint _initialPrize,
-	// 	uint _bidPrice,
-	// 	uint _bidFeePct,
-	// 	uint _bidAddBlocks,
-	// 	uint _initialBlocks
-	// ) public payable returns (address _addr);
 	function lastCreatedAuction() public constant returns (address _auction);
 }
