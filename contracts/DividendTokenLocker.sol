61b70cdf773df09241deb5c61c0ef6fef7ec035b
Penny Ether Team <pennyether@users.noreply.github.com>
2018-02-16 01:46:02 +0000

    - Changed DividendTokenLocker to do vesting - Added tests for DividendTokenLocker

--- contracts/DividendTokenLocker.sol
+++ contracts/DividendTokenLocker.sol
@@ -4,34 +4,142 @@ pragma solidity ^0.4.19;
 *************** DIVIDEND TOKEN LOCKER ********************
 **********************************************************
 
-This contract holds a balance of tokens. Those tokens
-cannot be transferred or burned.
+This contract holds a balance of tokens, and allows the owner:
+	- to collect all dividends
+	- to transfer tokens, such that some minimum balance
+	  is maintained, as defined by the vesting parameters
 
-Locker does only one thing, via the owner calling .collect():
-  	- call ".collectDividends()" on the DividendToken
-  	- send entire balance to an unchangeable "owner"
+Notes:
+	- Only the creator can specify the number of vesting days.
+	- If additional tokens are added, they can be transferred.
 */
 contract IDividendToken {
-	function collectDividends() public returns (uint _amount);
+	function collectOwedDividends() public returns (uint);
+	function transfer(address _to, uint _value) public;
+	function balanceOf(address _addr) public returns (uint);
 }
 contract DividendTokenLocker {
+	// set by creator in the constructor
+	address public creator;
 	address public owner;
-	address public token;
+	IDividendToken public token;
+	// set by creator via .setVesting()
+	uint public vestingAmt;
+	uint public vestingStartDay;
+	uint public vestingDays;
+
+	// events, for transparency
+	event Initialized(uint time, address creator, address token, address owner);
+	event VestingStarted(uint time, uint numTokens, uint vestingDays);
+	event Transferred(uint time, address recipient, uint numTokens);
+	event Collected(uint time, address recipient, uint amount);
 	
+	// Initialize the creator, token, and owner addresses.
     function DividendTokenLocker(address _token, address _owner)
     	public
     {
-		token = _token;
+    	creator = msg.sender;
+		token = IDividendToken(_token);
 		owner = _owner;
+		Initialized(now, creator, token, owner);
 	}
 
+	// Allow this contract to get sent Ether (eg, dividendsOwed)
 	function () payable public {}
 
+
+	/***************************************************/
+	/*********** CREATOR FUNCTIONS *********************/
+	/***************************************************/
+
+	// Starts the vesting process for the current balance.
+	// TokenLocker will ensure a minimum balance is maintained
+	//  based off of the vestingAmt and vestingDays.
+	function startVesting(uint _vestingDays) {
+		require(msg.sender == creator);
+		uint _numTokens = token.balanceOf(this);
+		vestingAmt = _numTokens;
+		vestingStartDay = today();
+		vestingDays = _vestingDays;
+		VestingStarted(now, _numTokens, _vestingDays);
+	}
+
+
+	/***************************************************/
+	/*********** OWNER FUNCTIONS ***********************/
+	/***************************************************/
+
+	// Allows the owner to collect the balance of this contract,
+	//  including any owed dividends.
 	function collect()
 		public
 	{
 		require(msg.sender == owner);
-		IDividendToken(token).collectDividends();
-		require(owner.call.value(this.balance)());
+		// Collect dividends, and get new balance.
+		token.collectOwedDividends();
+		uint _amount = this.balance;
+
+		// Send amount (if any), emit event.
+		if (_amount > 0){
+			require(owner.call.value(_amount)());
+		}
+		Collected(now, owner, _amount);
 	}
+
+	// Allows the owner to transfer tokens, such that the
+	//  balance of tokens cannot go below getMinTokenBalance().
+	function transfer(address _to, uint _numTokens)
+		public
+	{
+		require(msg.sender == owner);
+		// token.balanceOf() and getMinTokenBalance() can never be greater than
+		//   all the Ether in the world, so we dont worry about overflow.
+		int _maxAllowed = int(token.balanceOf(this)) - int(getMinTokenBalance());
+
+		// Set _maxAllowed and _numTokens to be 0 <= x <= balance
+		if (_maxAllowed < 0) _maxAllowed = 0;
+		if (_numTokens > uint(_maxAllowed)) _numTokens = uint(_maxAllowed);
+
+		// Transfer (if _numTokens > 0), and emit event.
+		if (_numTokens > 0) {
+			token.transfer(_to, _numTokens);
+		}
+		Transferred(now, _to, _numTokens);
+	}
+
+
+	/***************************************************/
+	/*********** VIEWS *********************************/
+	/***************************************************/
+
+	// Returns the minimum allowed tokenBalance.
+	// Starts at vestingAmt, goes to 0 after vestingDays.
+	function getMinTokenBalance()
+		public
+		view
+		returns (uint)
+	{
+		return vestingAmt - getNumTokensVested();
+	}
+
+	// Returns how many tokens have vested.
+	// Starts at 0, goes to vestingAmt after vestingDays.
+	function getNumTokensVested()
+		public
+		view
+		returns (uint)
+	{
+		uint _daysElapsed = today() - vestingStartDay;
+		if (_daysElapsed >= vestingDays) return vestingAmt;
+		else return (vestingAmt * _daysElapsed) / vestingDays;
+	}
+
+	// Returns the current day.
+	function today()
+  		private 
+  		view 
+  		returns (uint)
+  	{
+    	return now / 1 days;
+  	}
 }
---
+++ tests/DividendTokenLocker.js
@@ -0,0 +1,244 @@
+const DividendTokenLocker = artifacts.require("DividendTokenLocker");
+const DividendToken = artifacts.require("DividendToken");
+
+const createDefaultTxTester = require("../js/tx-tester/tx-tester.js")
+    .createDefaultTxTester.bind(null, web3, assert, it);
+const testUtil = createDefaultTxTester().plugins.testUtil;
+const BigNumber = web3.toBigNumber(0).constructor;
+
+const accounts = web3.eth.accounts;
+const creator = accounts[1];
+const owner = accounts[2];
+const anon = accounts[3];
+var token, locker;
+
+const VESTING_AMT = new BigNumber(6e18);
+const VESTING_DAYS = new BigNumber(600);
+var TODAY;  // the day that .startVesting() is called
+
+describe('DividendTokenLocker', function(){
+    before("Initialize DividendToken and DividendTokenLocker", async function(){
+        const addresses = {
+            creator: creator,
+            owner: owner,
+            anon: anon
+        };
+        await createDefaultTxTester().nameAddresses(addresses).start();
+
+        this.logInfo("Create a DividendToken, owned by creator.");
+        await createDefaultTxTester()
+            .doNewTx(DividendToken, [], {from: creator})
+            .assertSuccess()
+            .withTxResult((res, plugins)=>{
+                token = res.contract;
+                plugins.addAddresses({dividendToken: token.address});
+            }).start();
+
+        this.logInfo("Create a DividendTokenLocker");
+        await createDefaultTxTester()
+            .doNewTx(DividendTokenLocker, [token.address, owner], {from: creator})
+            .assertSuccess()
+            .assertOnlyLog("Initialized",{
+                creator: creator,
+                token: token.address,
+                owner: owner
+            })
+            .withTxResult((res, plugins)=>{
+                locker = res.contract;
+                plugins.addAddresses({locker: locker.address});
+            }).start();
+
+        await createDefaultTxTester().printNamedAddresses().start();
+    });
+
+    describe("Initial values", function(){
+        it("Has proper addresses", function(){
+            return createDefaultTxTester()
+                .assertCallReturns([locker, "creator"], creator)
+                .assertCallReturns([locker, "token"], token.address)
+                .assertCallReturns([locker, "owner"], owner)
+                .start();
+        });
+        it("Has empty vesting params", function(){
+            return createDefaultTxTester()
+                .assertCallReturns([locker, "vestingAmt"], 0)
+                .assertCallReturns([locker, "vestingStartDay"], 0)
+                .assertCallReturns([locker, "vestingDays"], 0)
+                .start();
+        });
+        it(".getMinTokenBalance() should be 0, since nothing is vesting", function(){
+            return createDefaultTxTester()
+                .assertCallReturns([locker, "getMinTokenBalance"], 0)
+                .start();
+        })
+    });
+
+    describe(".startVesting()", function(){
+        before("Mint some tokens for TokenLocker", function(){
+            return createDefaultTxTester()
+                .doTx([token, "mintTokens", locker.address, VESTING_AMT, {from: creator}])
+                .assertSuccess()
+                .assertCallReturns([token, "balanceOf", locker.address], VESTING_AMT)
+                .start();
+        });
+        it("Cannot be called by anon", function(){
+            return createDefaultTxTester()
+                .doTx([locker, "startVesting", VESTING_DAYS, {from: anon}])
+                .assertInvalidOpCode()
+                .start();
+        });
+        it("Cannot be called by owner", function(){
+            return createDefaultTxTester()
+                .doTx([locker, "startVesting", VESTING_DAYS, {from: owner}])
+                .assertInvalidOpCode()
+                .start();
+        });
+        it("Can be called by creator", function(){
+            return createDefaultTxTester()
+                .doTx([locker, "startVesting", VESTING_DAYS, {from: creator}])
+                .assertSuccess()
+                .assertOnlyLog("VestingStarted", {
+                    numTokens: VESTING_AMT,
+                    vestingDays: VESTING_DAYS
+                })
+                .withTxResult((res, plugins)=>{
+                    TODAY = today();
+                })
+                .assertCallReturns([locker, "vestingAmt"], VESTING_AMT)
+                .assertCallReturns([locker, "vestingDays"], VESTING_DAYS)
+                .assertCallReturns([locker, "vestingStartDay"], ()=>TODAY)
+                .start();
+        });
+    });
+
+    describe(".transfer() should not be callable by anon", function(){
+        return createDefaultTxTester()
+            .doTx([locker, "transfer", anon, 1e18, {from: anon}])
+            .assertInvalidOpCode()
+            .start();
+    });
+
+    describe("Make sure vesting works", function(){
+        it("Initially, nothing can be transferred", function(){
+            return assertCanTransfer(anon, 1e18);
+        });
+        it("Fast forward 1 day", function(){
+            testUtil.fastForward(24*60*60);
+        });
+        it("Can transfer 1 days worth of vested tokens", function(){
+            return assertCanTransfer(anon, 1e18);
+        });
+        it("Fast forward 3 days", function(){
+            testUtil.fastForward(3*24*60*60);
+        });
+        it("Can transfer 3 days worth of vested tokens", function(){
+            return assertCanTransfer(anon, 1e18);
+        });
+    });
+
+    describe("Adding additional tokens to balance", function(){
+        this.logInfo("If additional tokens are transferred to locker");
+        this.logInfo("They should be transferrable, regardless of vesting.");
+        it("Transfer as many vested tokens as possible", function(){
+            return assertCanTransfer(anon, 10e18);
+        });
+        it("Mint another 1e18 tokens to locker", function(){
+            return createDefaultTxTester()
+                .doTx([token, "mintTokens", locker.address, 1e18, {from: creator}])
+                .assertSuccess()
+                .start();
+        });
+        it("Can now transfer .5e18 tokens", function(){
+            return assertCanTransfer(anon, .5e18);
+        });
+        it("FastForward 1 day", function(){
+            testUtil.fastForward(1*24*60*60);
+        });
+        it("Should now transfer .5e18 + 1-day-vested", function(){
+            return assertCanTransfer(anon, 10e18);
+        });
+    });
+
+    describe(".collectDividends()", function(){
+        const DIV_AMT = new BigNumber(1e18);
+        it("Send dividend to DividendToken", function(){
+            return createDefaultTxTester()
+                .doTx([token, "sendTransaction", {from: anon, value: DIV_AMT}])
+                .assertSuccess()
+                .start();
+        });
+        it("Should not be callable by anon", function(){
+            return createDefaultTxTester()
+                .doTx([locker, "collect", {from: anon}])
+                .assertInvalidOpCode()
+                .start();
+        });
+        it("Should collect its share of dividends", async function(){
+            const balance = await token.balanceOf(locker.address);
+            const totalSupply = await token.totalSupply();
+            const expAmt = DIV_AMT.div(totalSupply).mul(balance).floor();
+            const pct = balance.div(totalSupply).mul(100).toFixed(2);
+            this.logInfo(`Locker owns ${pct}% of tokens.`);
+            this.logInfo(`Should send ${expAmt} Eth to owner`);
+
+            return createDefaultTxTester()
+                .startLedger([owner, token])
+                .doTx([locker, "collect", {from: owner}])
+                .stopLedger()
+                .assertSuccess()
+                .assertOnlyLog("Collected", {
+                    recipient: owner,
+                    amount: expAmt
+                })
+                .assertDeltaMinusTxFee(owner, expAmt)
+                .assertDelta(token, expAmt.mul(-1))
+                .start();
+        });
+    });
+
+    async function assertCanTransfer(to, amt) {
+        amt = new BigNumber(amt);
+        // assert vesting is correct
+        const startDay = await locker.vestingStartDay();
+        const vestingAmt = await locker.vestingAmt();
+        const vestingDays = await locker.vestingDays();
+        // compute expAmtVested and expMinBalance
+        const daysElapsed = BigNumber.min(today().minus(startDay), vestingDays);
+        const expAmtVested = vestingAmt.mul(daysElapsed).div(vestingDays);
+        const expMinValance = vestingAmt.minus(expAmtVested);
+        // compute how many tokens _should_ be transferred
+        const balance = await token.balanceOf(locker.address);
+        const minBalance = await locker.getMinTokenBalance();
+        const expAmt = balance.minus(amt).gt(minBalance)
+            ? amt
+            : BigNumber.max(balance.minus(minBalance), 0);
+        const expBalance = balance.minus(expAmt);
+        // compute expToBalance
+        const expToBalance = (await token.balanceOf(to)).plus(expAmt);
+
+        // print useful info
+        console.log(`Has vested ${daysElapsed} days: ${t(expAmtVested)}.`);
+        console.log(`Min Balance is: ${t(minBalance)}`);
+        if (expAmt.lt(amt)) {
+            console.log(`Should not be able to transfer all: ${t(amt)}`);
+        }
+        console.log(`Should transfer: ${t(expAmt)}`);
+
+        return createDefaultTxTester()
+            .doTx([locker, "transfer", to, amt, {from: owner}])
+            .assertSuccess()
+            .assertOnlyLog("Transferred", {
+                recipient: to,
+                numTokens: expAmt
+            })
+            .assertCallReturns([token, "balanceOf", locker.address], expBalance)
+            .assertCallReturns([token, "balanceOf", to], expToBalance)
+            .start();
+    }
+    function t(amt){
+        return `${amt.div(1e18).toFixed(5)} Tokens`;
+    }
+    function today() {
+        return (new BigNumber(testUtil.getBlockTime())).div(24*60*60).floor();
+    }
+});
